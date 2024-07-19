24508ba2b6beddbd3f34ce14d7ea4a303e556fc8
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-07-19 06:57:07 -0400

    add c n o

--- steps.js
+++ steps.js
@@ -1,7 +1,15 @@
-let stepCount = 0;
+var stepCount = 0;
+
+if(localStorage.getItem("steps") != null){
+     stepCount = localStorage.getItem("steps");
+
+}
 let threshold = 12; // Adjust this value based on sensitivity
 let previousMagnitude = 0;
-
+function alerter(){
+    const stepnot = new Notification("Your steps are currently " + stepCount);
+    setTimeout(alerter, 86400000);
+}
 if ('Accelerometer' in window) {
     try {
         const accelerometer = new Accelerometer({ frequency: 20 });
@@ -15,13 +23,31 @@ if ('Accelerometer' in window) {
 
             if (Math.abs(magnitude - previousMagnitude) > threshold) {
                 stepCount++;
+                document.querySelector("progress").value = steps;
+                if(document.querySelector("progress").value >= 410549) const finished = new Notification("You walked the canal!");
                 document.getElementById('steps').textContent = stepCount;
+                localStorage.setItem("steps", stepCount)j
             }
 
             previousMagnitude = magnitude;
         });
 
         accelerometer.start();
+        async function notifyMe() {
+            if (!("Notification" in window)) {
+            } else if (Notification.permission !== "denied") {
+              // We need to ask the user for permission
+              Notification.requestPermission().then((permission) => {
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    alerter();
+                }
+              });
+            }
+          
+            // At last, if the user has denied notifications, and you
+            // want to be respectful there is no need to bother them anymore.
+          }
     } catch (error) {
         console.error('Accelerometer not supported:', error);
         document.getElementById('steps').textContent = 'Accelerometer not supported.';
@@ -29,3 +55,4 @@ if ('Accelerometer' in window) {
 } else {
     document.getElementById('steps').textContent = 'Accelerometer not supported on this device.';
 }
+
