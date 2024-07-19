95f9c45079845e1542cbddd587af612ad03d1531
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-07-19 12:30:18 -0400

    change threshold

--- steps.js
+++ steps.js
@@ -4,7 +4,7 @@ if(localStorage.getItem("steps") != null){
      stepCount = localStorage.getItem("steps");
 
 }
-let threshold = 12; // Adjust this value based on sensitivity
+let threshold = 4; // Adjust this value based on sensitivity
 let previousMagnitude = 0;
 function alerter(){
     const stepnot = new Notification("Your steps are currently " + stepCount);
